5189f19ef54aa4c769d86d0955c6ef4983317fce
henry <henry@DESKTOP-PABF9VO>
2022-09-21 08:26:38 +0700

    domotu

---
+++ src/main/java/Student.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author henry
+ */
+public class Student {
+    private int studentID;
+    private String studentName;
+    
+    
+    public Student(){
+        studentID = 1;
+        studentName ="Ton lan";
+    }
+    
+}
